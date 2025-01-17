5eceaea760e6857c62125d7582a60489451bd6ab
mukeshvijayakumar <mukesh@techasoft.com>
2025-01-17 12:40:39 +0530

    Projects and ProjectCard component added

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,7 +7,6 @@ import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
 import colorSharp from "../assets/img/color-sharp.png";
-import colorSharp2 from "../assets/img/color-sharp2.png";
 
 export const Skills = () => {
   const responsive = {
