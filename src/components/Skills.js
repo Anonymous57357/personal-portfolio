b4e76b07f933d13f25149109b24f238fd863adcd
mukeshvijayakumar <mukesh@techasoft.com>
2025-01-18 14:58:32 +0530

    add animation && footer && mailchimpForm && fix minor bugs

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,10 @@
-import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
-
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import arrow1 from "../assets/img/arrow1.svg";
+import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
@@ -28,50 +27,46 @@ export const Skills = () => {
       items: 1,
     },
   };
+
   return (
     <section className="skill" id="skills">
-      <Container>
-        {" "}
-        <Row>
-          <Col>
-            <div className="skill-bx">
-              <h2> skills</h2>
-              <p>This is about skills section</p>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Skills</h2>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.<br></br> Lorem Ipsum has been the industry's standard
+                dummy text.
+              </p>
               <Carousel
                 responsive={responsive}
                 infinite={true}
-                className="skill-slider"
+                className="owl-carousel owl-theme skill-slider"
               >
                 <div className="item">
-                  <img src={meter1} alt="Image"></img>
+                  <img src={meter1} alt="Image" />
                   <h5>Web Development</h5>
                 </div>
                 <div className="item">
-                  <img src={meter2} alt="Image"></img>
+                  <img src={meter2} alt="Image" />
                   <h5>Brand Identity</h5>
                 </div>
                 <div className="item">
-                  <img src={meter3} alt="Image"></img>
-                  <h5>Backend Development</h5>
-                </div>
-                <div className="item">
-                  <img src={meter1} alt="Image"></img>
-                  <h5>Frontend Development</h5>
-                </div>
-                <div className="item">
-                  <img src={meter2} alt="Image"></img>
+                  <img src={meter3} alt="Image" />
                   <h5>Logo Design</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="Image" />
+                  <h5>Web Development</h5>
                 </div>
               </Carousel>
             </div>
-          </Col>
-        </Row>
-      </Container>
-      <img
-        className="background-image-left"
-        src={colorSharp}
-        alt="colorsharp"
-      />
+          </div>
+        </div>
+      </div>
+      <img className="background-image-left" src={colorSharp} alt="Image" />
     </section>
   );
 };
