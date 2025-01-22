dcbfcbaa31d15743528b605828ec42dacfa02c0e
mukeshvijayakumar <mukesh@techasoft.com>
2025-01-22 11:52:38 +0530

    changes

--- src/components/constants.js
+++ src/components/constants.js
@@ -4,12 +4,17 @@ export const DEVTINDER_GITHUB_LINK =
 export const SWIGGY_STORE_GITHUB_LINK =
   "https://github.com/Anonymous57357/namaste-react/tree/main/Episode%2003%20-%20Laying%20The%20Foundation";
 
-export const NETFLIX_GEMINI = "https://github.com/Anonymous57357/netfilx-gpt";
-
 export const YOUTUBE_APP_GITHUB_LINK =
   "https://github.com/Anonymous57357/namste-youtube/tree/main/namaste-youtube";
 
-export const NETFLIX_GEMINI_LIVE_URL =
-  "https://peaceful-fairy-68eeba.netlify.app/";
+export const NETFLIX_GEMINI = "https://github.com/Anonymous57357/netfilx-gpt";
+
+export const DEVTINDER_LIVE_URL = "";
+
+export const SWIGGY_STORE_LIVE_URL =
+  "https://unrivaled-blancmange-8ed1d1.netlify.app/";
 
 export const YOUTUBE_APP_LIVE_URL = "https://youtubesimulator.netlify.app/";
+
+export const NETFLIX_GEMINI_LIVE_URL =
+  "https://peaceful-fairy-68eeba.netlify.app/";
